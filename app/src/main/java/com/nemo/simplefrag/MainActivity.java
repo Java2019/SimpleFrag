0876857cb0749a41d8cc6012d034940f5d8c2113
java2019 <java_2019@mail.ru>
2017-07-30 11:39:13 +0300

    init

--- app/src/main/java/com/nemo/simplefrag/MainActivity.java
+++ app/src/main/java/com/nemo/simplefrag/MainActivity.java
@@ -1,13 +1,34 @@
 package com.nemo.simplefrag;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new BlankFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
